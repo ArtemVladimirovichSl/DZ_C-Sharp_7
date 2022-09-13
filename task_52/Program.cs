61c4313aa8f4c4964100a8818474adb7cfd40da4
Artem <slivnitsin.art@mail.ru>
2022-09-13 22:45:49 +0800

    task_52

---
+++ task_52/Program.cs
@@ -0,0 +1,25 @@
+﻿int line = Convert.ToInt32(new Random().Next(3,6));
+int column = Convert.ToInt32(new Random().Next(3,6));
+double[,] matrix = new double[line, column];
+
+for (int i = 0; i < matrix.GetLength(0); i++) 
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = Convert.ToInt32(new Random().Next(0,100));  
+        Console.Write (matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int j = 0; j < matrix.GetLength(1); j++) 
+{
+    double summa = 0;
+    double average = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        summa += matrix[i, j];
+    }
+    average = summa/line;
+    Console.WriteLine("Среднее арифметическое столбца "+(j+1)+" равно: "+"{0,6:F2}", average);
+}
