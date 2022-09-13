0c2ebaaf277aedc40c0445a0aa7fc42b29779375
Artem <slivnitsin.art@mail.ru>
2022-09-14 00:52:44 +0800

    task50

--- task_50/Program.cs
+++ task_50/Program.cs
@@ -1,22 +1,26 @@
-﻿Console.WriteLine("Введите размеры массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[m, n];
+﻿Console.Write("Введите количество строк массива: ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[line, column];
 
 for (int i = 0; i < matrix.GetLength(0); i++) 
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-    matrix[i, j] = Convert.ToInt32(new Random().Next(0,100));  
+    matrix[i,j] = Convert.ToInt32(new Random().Next(0,100));  
     Console.Write (matrix[i,j] + " ");
     }
     Console.WriteLine();
 }
 
-Console.WriteLine("Введите позиции элемента: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию элемента по горизонтали: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию элемента по вертикали: ");
+int y = Convert.ToInt32(Console.ReadLine());
+y -= 1;
+x -= 1;
 
-if (a > m || b > n) Console.WriteLine("Такой позиции в массиве нет");
-else 
-Console.WriteLine($"Значение элемента = {matrix.GetValue(a,b)}");
+if(y < line && x < column) Console.WriteLine($"Значение элемента = {matrix.GetValue(y,x)}");
+else
+Console.WriteLine("Такого элемента в массиве нет");
