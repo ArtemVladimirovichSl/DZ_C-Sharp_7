0d1e217260553147a2d1606e04589b97068db4c0
Artem <slivnitsin.art@mail.ru>
2022-09-16 23:52:50 +0800

    task_47.2

--- task_47/Program.cs
+++ task_47/Program.cs
@@ -9,7 +9,7 @@ for(int i = 0; i < matrix.GetLength(0); i++)
 {
     for(int j = 0; j < matrix.GetLength(1); j++)
     {
-        matrix[i, j] = new Random().NextDouble() * 100;
+        matrix[i, j] = new Random().Next(-100, 100) + new Random().NextDouble();
         Console.Write(Math.Round(matrix[i, j], 2) + " ");
     }
 Console.WriteLine();
