34c57261e29c9e4c81327cfc78db76ddce20742a
Artem <slivnitsin.art@mail.ru>
2022-09-16 23:40:20 +0800

    task_47.1

--- task_47/Program.cs
+++ task_47/Program.cs
@@ -4,14 +4,13 @@ Console.WriteLine("Задайте количество столбцов двум
 int column = Convert.ToInt32(Console.ReadLine());
 
 double[,] matrix = new double[line, column];
-Random random = new Random();
 
 for(int i = 0; i < matrix.GetLength(0); i++)
 {
     for(int j = 0; j < matrix.GetLength(1); j++)
     {
-        matrix[i, j] = random.NextDouble() * 100;
-        Console.Write(" " + "{0,6:F2}", (matrix[i, j]));
+        matrix[i, j] = new Random().NextDouble() * 100;
+        Console.Write(Math.Round(matrix[i, j], 2) + " ");
     }
 Console.WriteLine();
 }
